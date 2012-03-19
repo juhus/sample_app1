fa162cc4a4c5795ed0f105f14f2a980f895960e4
juhus <soh.ju.hu@gmail.com>
2012-03-19 11:37:00 +0800

    6 basic test examples on static pages ended. Refactoring started.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title" do
+    	visit '/static_pages/home'
+    	page.should have_selector('title',
+    								:text => "Sample App | Home")
+    end
+
   end
 
   describe "Help page" do
@@ -16,6 +23,27 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+    	visit '/static_pages/help'
+    	page.should have_selector('title',
+    								:text => "Sample App | Help")
+    end
+
   end
-  
+
+  describe "About page" do
+  	it "should have the content 'About Us'" do
+  		visit '/static_pages/about'
+  		page.should have_content('About Us')
+  	end
+
+  	it "should have the right title" do
+    	visit '/static_pages/about'
+    	page.should have_selector('title',
+    								:text => "Sample App | About")
+    end
+
+  end
+
 end
