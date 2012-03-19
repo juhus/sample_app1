c7bad240088c5ab70bbdc3346f8cb447c8a7ecd6
juhus <soh.ju.hu@gmail.com>
2012-03-19 22:08:32 +0800

    Finish Chapter 5 Layout and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
@@ -55,6 +57,7 @@ SampleApp::Application.routes.draw do
   # just remember to delete public/index.html.
   root to:'static_pages#home'
 
+  match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
