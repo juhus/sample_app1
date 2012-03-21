f65997bc5ef729baa7c72d6f99ee7c590bcba30c
juhus <soh.ju.hu@gmail.com>
2012-03-21 14:34:01 +0800

    Finish Chapter 9 user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
       		sign_in user
-      		redirect_to user
+      		redirect_back_or user # redirect_back_or method uses the or operator || through
 		else
-		    flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+		    flash.now[:error] = 'Invalid email/password combination' 
       		render 'new'
 		end
 	end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
+    
   end
 end
+
+# invoke FactoryGirl.create(:user)
+# before(:all) { 30.times { FactoryGirl.create(:user) } }
+# after(:all)  { User.delete_all } 
